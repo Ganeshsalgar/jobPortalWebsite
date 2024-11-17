289d69f92be536e3c86d18fbe355a7d9f78469c9
Ganeshsalgar <ganeshsalgar2002@gmail.com>
2024-11-18 02:01:23 +0530

    fix login error

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -46,63 +46,62 @@ export const register = async (req , res) =>{
 }
 
 
-export const login = async (req , res) => {
+export const login = async (req, res) => {
     try {
-            const {email , password , role} = req.body;
-            if(!email || !password || !role){
-                return res.status(400).json({
-                    message : "Something is missing",
-                    success : false
-                });
-            }
-            const user = User.findOne({email});
-            if(!user){
-                return res.status(400).json({
-                    message: "Incorrect email or Password",
-                    success : false
-                })
-            }
-            const isPasswordMatch = await bcrypt.compare(password, user.password);
-            if(!isPasswordMatch){
-                return res.status(400).json({
-                    message: "Incorrect email or Password",
-                    success : false
-                })
-            } 
-
-            // check role is correct or not
-            if(role !== user.role){
-                return res.status(400).json({
-                    message: "Account Doesn't exist with current role",
-                    success : false
-                })
-            }
-            
-            const tokenData = {
-                userId : user._id
-            }
-
-            const token  = await jwt.sign(tokenData , process.env.SECRETE_KEY,{expiresIn :'1d'});
-
-            user = {
-                _id: user._id,
-                fullname : user.fullname,
-                email: user.email,
-                phoneNumber : user.phoneNumber,
-                role: user.role,
-                profile: user.profile
-            }
-            return res.status(200).cookie("token" , token, {maxAge : 1*24*60*60*1000 , httpsOnly: true ,sameSite : 'strict'}).json({
-               message : `Welcome back ${user.fullname}`,
-               success: true 
-            })
-
-
-    } catch (error) {
-        console.log("User login is failed please enter the valid Credientials" , error);
+        const { email, password, role } = req.body;
         
+        if (!email || !password || !role) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            });
+        };
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "Incorrect email or password.",
+                success: false,
+            })
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                message: "Incorrect email or password.",
+                success: false,
+            })
+        };
+        // check role is correct or not
+        if (role !== user.role) {
+            return res.status(400).json({
+                message: "Account doesn't exist with current role.",
+                success: false
+            })
+        };
+
+        const tokenData = {
+            userId: user._id
+        }
+        const token = jwt.sign(tokenData, process.env.SECRETE_KEY, { expiresIn: '1h' });
+
+
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+        }
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Welcome back ${user.fullname}`,
+            user,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
     }
-} 
+}
 
 export const logout = async (req, res) => {
     try {
