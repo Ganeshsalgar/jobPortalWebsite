341da2566c161752953f10c7bb61b7ad1cd51bca
Ganeshsalgar <ganeshsalgar2002@gmail.com>
2024-11-25 01:45:14 +0530

    work  and logic done to register  and update company

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import CompaniesTable from './CompaniesTable'
 import { useNavigate } from 'react-router-dom'
+import useGetAllCompanies from '@/hooks/useGetAllCompanies'
+import { useDispatch } from 'react-redux'
+import { setSearchCompanyByText } from '@/redux/companySlice'
 
 const Companies = () => {
+    useGetAllCompanies();
+    const [input ,setInput] = useState("");
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    useEffect(() =>{
+        dispatch(setSearchCompanyByText(input));
+    } , [input])
+    
   return (
     <div>
         <Navbar />
@@ -15,6 +26,7 @@ const Companies = () => {
             <Input 
                 className="w-fit"
                 placeholder="Filter by Name"
+                onChange={(e) => setInput(e.target.value)}
             />
             <Button onClick={() => navigate("/admin/companies/create")}>
                 New Company
--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -1,29 +1,26 @@
-import React, { useEffect } from 'react'
+import { setSingleCompany } from '@/redux/companySlice'
 import { setAllJobs } from '@/redux/jobSlice'
+import { COMPANY_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
+import axios from 'axios'
+import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import axios from 'axios';
-import { COMPANY_API_END_POINT } from '@/utils/constant';
-import { setSingleCompany } from '@/redux/companySlice';
 
-
-const useGetCompanyById = () => {
+const useGetCompanyById = (companyId) => {
     const dispatch = useDispatch();
-
-    useEffect(() => {
+    useEffect(()=>{
         const fetchSingleCompany = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}` , {withCredentials:true})
-
-                if(res.data.message){
-                    dispatch(setSingleCompany(res.data.company))
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
+                console.log(res.data.company);
+                if(res.data.success){
+                    dispatch(setSingleCompany(res.data.company));
                 }
             } catch (error) {
                 console.log(error);
-                
             }
         }
         fetchSingleCompany();
-    } ,[companyId , dispatch])
+    },[companyId, dispatch])
 }
 
-export default useGetCompanyById;
+export default useGetCompanyById
